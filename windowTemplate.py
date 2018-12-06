c7368a9c2f79a84d1ee757ad9acc6e1c864edb46
gsproston <gsproston94@gmail.com>
2018-12-06 21:46:48 +0000

    GoL update

--- windowTemplate.py
+++ windowTemplate.py
@@ -1,71 +1,55 @@
 import pygame, sys
 from pygame.locals import *
 
-pygame.init()
+# window constants
+RATIOS = ["16:9","16:10","4:3"] # ratios
+RES169 = [[1024,576],[1152,648],[1280,720],[1366,768],[1600,900],[1920,1080]]
+RES1610 = [[1280,800],[1440,900],[1680,1050]]
+RES43 = [[960,720],[1024,768],[1280,960],[1400,1050],[1440,1080],[1600,1200],[1856,1392]]
+RESS = [RES169,RES1610,RES43] # holds various resolution options
 
-def draw(): #called when screen needs to be updated
-    pygame.display.flip()
-    
-def wresize(): #screen is resized, recalculate some variables
-    global cxpos,cypos
-    cxpos = int(screen.get_width()/2)
-    cypos = int(screen.get_height()/2)
-
-def resetFlags(): #new flags set
-    if fscreen:
-        w = wInfo.current_w
-        h = wInfo.current_h
-        if bwindow:
-            flags = FULLSCREEN|NOFRAME
-        else:
-            flags = FULLSCREEN
+# new flags set
+# returns the new screen object
+def resetFlags(fscreen,bwindow): 
+  # gets monitor info, used when resizing
+  wInfo = pygame.display.Info()
+  if fscreen:
+    w = wInfo.current_w
+    h = wInfo.current_h
+    if bwindow:
+      flags = FULLSCREEN|NOFRAME
     else:
-        w = windowWidth
-        h = windowHeight
-        if bwindow:
-            flags = NOFRAME
-        else:
-            flags = 0
-    pygame.display.set_mode((w,h),flags)
-    
-if __name__ == "__main__":
-    #gets monitor info, used when resizing
-    wInfo = pygame.display.Info()
+      flags = FULLSCREEN
+  else:
+    w = windowWidth
+    h = windowHeight
+    if bwindow:
+      flags = NOFRAME
+    else:
+      flags = 0
+  pygame.display.set_mode((w,h),flags)
+  
+def getRatio():
+  return RATIOS[ratind]
+  
+def getResolution():
+  return RESS[ratind][resind]
 
-    #variables
-    shutdown = False
-    windowWidth = 1024
-    windowHeight = 576
-    flags = 0
-    #menu variables
-    fscreen = False #fullscreen
-    bwindow = False #borderless
-    RATIOS = ["16:9","16:10","4:3"] #ratios
-    RESS = [[],[],[]] #holds various resolution options
-    RES169 = [[1024,576],[1152,648],[1280,720],[1366,768],[1600,900],[1920,1080]]
-    RES1610 = [[1280,800],[1440,900],[1680,1050]]
-    RES43 = [[960,720],[1024,768],[1280,960],[1400,1050],[1440,1080],[1600,1200],[1856,1392]]
-    RESS[0] = RES169
-    RESS[1] = RES1610
-    RESS[2] = RES43
+def init():
+  global ratind, resind
+  print("Initialising the display")
+  # variables
+  flags = 0
+  # menu variables
+  fscreen = False # fullscreen
+  bwindow = False # borderless
+  ratind = 0 # index of the ratio menu
+  resind = 2 # index of the resolution menu
+  # ratios and resolutions can be found in constants
+  windowWidth = RESS[ratind][resind][0]
+  windowHeight = RESS[ratind][resind][1]
 
-    #init screen
-    screen = pygame.display.set_mode((windowWidth, windowHeight),flags)
-    cxpos = int(screen.get_width()/2)
-    cypos = int(screen.get_height()/2)
-    screen.fill(Color(255,255,255))
-    
-    clock = pygame.time.Clock()
-
-    draw()
-
-    #main game loop
-    while (not shutdown):        
-        for event in pygame.event.get(): #runs when an event occurs
-            if event.type == QUIT: #quit called
-                shutdown = True #end loop
-
-        clock.tick(144) #update x times a second, determines FPS
-
-    #main loop ends, exit
-    pygame.quit()    
+  # init screen
+  screen = pygame.display.set_mode((windowWidth, windowHeight),flags)
+  pygame.display.set_caption("Snap")
+  screen.fill(Color(255,255,255))
